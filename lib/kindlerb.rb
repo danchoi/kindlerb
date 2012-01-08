8c08f5aa3ed0237801c6a5478ea60c2fde3f5e44
Daniel Choi <dhchoi@gmail.com>
2012-01-08 12:59:44 -0500

    extract nav structure from file tree

--- lib/kindlerb.rb
+++ lib/kindlerb.rb
@@ -1,6 +1,6 @@
-
-
 =begin
+
+kindlerb
 
 kindlerb converts a file tree of sections, articles, images, and metadata into
 a MOBI periodical-formatted document for the Kindle. It is a wrapper around the
@@ -11,7 +11,7 @@ Make sure kindlegen is on the PATH.
 
 Run the program at the root of the file tree:
 
-    kindlerb
+    kindlerb [filetree dir]
 
 The output will be a mobi document.
 
@@ -29,9 +29,33 @@ The tree structure is
 kindlerb will extract article titles from the *.html files and create the NCX
 from that. (DRY)
 
-
-
 =end
 
+# extract nav structure
 
+require 'pathname'
+require 'yaml'
+require 'nokogiri'
 
+target_dir = Pathname.new(ARGV.first || '.')
+
+Dir.chdir target_dir do
+  Dir['sections/*'].entries.sort.map {|section_dir| 
+    puts section_dir
+    section = {
+      :section_meta => YAML::load_file((Pathname.new(section_dir) + '_section.yml')),
+      :articles => 
+        Dir[Pathname.new(section_dir) + '*'].entries.select {|x| x !~ /_section.yml/}.sort.map {|article_file|
+          doc = Nokogiri::HTML(File.read(article_file))
+          {
+            :file => article_file,
+            :title => doc.search("html/head/title").map(&:inner_text).first,
+            :author => doc.search("html/head/meta[@name=author]").map{|n|n[:name]}.first,
+            :description => doc.search("html/head/meta[@name=description]").map{|n|n[:content]}.first
+          }
+        }
+    }
+    puts section.to_yaml
+  }
+
+end
