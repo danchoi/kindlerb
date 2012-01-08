238cd8e4402f42b37efa5a2c49d7aa20c5545f52
Daniel Choi <dhchoi@gmail.com>
2012-01-08 12:33:07 -0500

    initial file with description of program

---
+++ lib/kindlerb.rb
@@ -0,0 +1,30 @@
+
+
+=begin
+
+kindlerb converts a file tree of sections, articles, images, and metadata into
+a MOBI periodical-formatted document for the Kindle. It is a wrapper around the
+kindlegen program from Amazon that hides the details for templating OPF and NCX
+files.
+
+Make sure kindlegen is on the PATH.
+
+The tree structure is 
+
+  opf.yml
+  sections/
+    000/
+      section.yml # contains section title
+      001.html # an article
+  media/
+    001.jpg
+    002.jpg
+
+kindlerb will extract article titles from the *.html files and create the NCX
+from that. (DRY)
+
+
+=end
+
+
+
